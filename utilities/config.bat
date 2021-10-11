c61fc1ee0003e1e4380e7a9286e8eb16a77e10b0
Joey Nigro <34990239+joeynigro@users.noreply.github.com>
2021-10-11 16:58:50 -0500

    PonyAnimate

---
+++ utilities/config.bat
@@ -0,0 +1,21 @@
+setlocal
+if "%SUBSCRIPT%"=="" ( start notepad.exe "%CD%\%~nx0" & exit )
+endlocal
+
+:: verbose
+set VERBOSEWRAPPER=n
+
+:: skip check depends
+set SKIPCHECKDEPENDS=y
+
+:: skip depend install
+set SKIPDEPENDINSTALL=n
+
+:: dev mode
+set DEVMODE=n
+
+:: dry run
+set DRYRUN=n
+
+:: port
+set PORT=4343
